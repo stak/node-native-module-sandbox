434321fe3215c9d82561e0d2b779ed9ac9ee8c1e
Shutaro Takimoto <shu@tak.im>
2016-05-18 20:25:13 +0900

    Implement add

--- addAsync.cc
+++ addAsync.cc
@@ -1,12 +1,25 @@
 #include <nan.h>
 
-void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-	info.GetReturnValue().Set(Nan::New("world").ToLocalChecked());
+NAN_METHOD(Add) {
+	if (info.Length() < 2) {
+		Nan::ThrowTypeError("Wrong number of arguments");
+		return;
+	}
+	if (!info[0]->IsNumber() || !info[1]->IsNumber()) {
+		Nan::ThrowTypeError("Wrong type of arguments");
+		return;
+	}
+
+	double arg0 = info[0]->NumberValue();
+	double arg1 = info[1]->NumberValue();
+	v8::Local<v8::Number> num = Nan::New(arg0 + arg1);
+
+	info.GetReturnValue().Set(num);
 }
 
-void Init(v8::Local<v8::Object> exports) {
-	exports->Set(Nan::New("hello").ToLocalChecked(),
-			     Nan::New<v8::FunctionTemplate>(Method)->GetFunction());
+NAN_MODULE_INIT(Init) {
+	Nan::Set(target, Nan::New("add").ToLocalChecked(),
+			 Nan::GetFunction(Nan::New<v8::FunctionTemplate>(Add)).ToLocalChecked());
 }
 
 NODE_MODULE(addAsync, Init)
--- test/test.js
+++ test/test.js
@@ -1,4 +1,7 @@
 const assert = require('assert');
 const addon = require('bindings')('addAsync');
 
-assert.equal(addon.hello(), "world");
+assert.equal(addon.add(1, 1), 2);
+assert.equal(addon.add(3, 5), 8);
+assert.equal(addon.add(-2, -3), -5);
+assert.equal(addon.add(0, 0), 0);
